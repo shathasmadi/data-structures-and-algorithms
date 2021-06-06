204b54321682d4ae4eb85e32f58c0dca08938af4
shathasmadi <alsmadishatha@gmail.com>
2021-06-06 18:47:32 +0300

    Solve the challanges

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -25,7 +25,7 @@ Use `forEach` to loop over the input array. Modify each string, and add the upda
 
 const addExclamation = (arr) => {
   let newArr=[];
-  arr.forEach(add => newArr.push(`${add}!`));
+  arr.forEach(element => newArr.push(`${element}!`));
   return newArr;};
 
 
@@ -130,7 +130,19 @@ Return the resulting output array.
 ------------------------------------------------------------------------------------------------ */
 
 const fizzbuzz = (arr) => {
-  // Solution code here...
+  let newArr=[];
+  arr.forEach(element => {
+    if (element % 3 ===0 && element % 5 !==0) {
+      newArr.push('Fizz');
+    }else if(element % 5 ===0 && element % 3 !==0){
+      newArr.push('Buzz');
+    }else if(element % 3 ===0 && element % 5 ===0){
+      newArr.push('Fizz Buzz');
+    }else{
+      newArr.push(element);
+    }
+  });
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -184,7 +196,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   const inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
   test('It should print out messages or numbers', () => {
